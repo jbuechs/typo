c0acb9291ce974833d66e7633bc5f73ec615364d
Jennie Buechner <janina.buechner@gmail.com>
2016-03-31 20:50:00 -0700

    only query all articles when admin and editing

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'pry'
 
 module Admin; end
 class Admin::ContentController < Admin::BaseController
@@ -146,7 +147,7 @@ class Admin::ContentController < Admin::BaseController
   def real_action_for(action); { 'add' => :<<, 'remove' => :delete}[action]; end
 
   def new_or_edit
-    @articles = Article.all
+    @articles = Article.all if current_user.admin? && params[:action] == "edit"
     id = params[:id]
     id = params[:article][:id] if params[:article] && params[:article][:id]
     @article = Article.get_or_build_article(id)
